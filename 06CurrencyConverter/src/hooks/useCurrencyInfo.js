12f2c0d00d0248828008155b5612df1948b2ef89
AniketDubey199 <aniketdubey0124@gmail.com>
2026-02-01 19:41:11 +0530

    update new project

---
+++ 06CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useState , useEffect } from "react";
+
+function useCurrencyInfo(currency){
+    const [data , setData] = useState({})
+
+    useEffect(() =>{
+        fetch(`https://api.frankfurter.app/latest?from=${currency.toUpperCase()}`)
+        .then((res) => res.json())
+        .then((res) => setData(res.rates));
+        console.log(data);
+    } , [currency])
+    return data
+}
+
+export default useCurrencyInfo; 
